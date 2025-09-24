51ad12391e0e65b8fc555237d679b7414911feaf
Leone Lima <leonnylimas@gmail.com>
2025-09-24 19:10:16 -0300

    adicionando filtros

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,8 @@
+import { parseRoutePath } from "../utils/parseRoutePath.js"
 import { tickets } from "./tickets.js"
 
-export const routes = [...tickets]
+export const routes = [...tickets].map((route) => ({
+    ...route,
+    path: parseRoutePath(route.path),
+
+}))
